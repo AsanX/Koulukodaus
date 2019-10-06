94f5bdbacadd01ae47d43105f1f9791aafd5fc7c
Santtu Krouvi <45737349+AsanX@users.noreply.github.com>
2019-10-06 17:22:17 +0300

    update
    Added bonus

--- SKR/5/h5_tehtava_4.cpp
+++ SKR/5/h5_tehtava_4.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 
-//#define Virallinen
-
 #define tuomarit 5+1
 #define ltulos 6
 int main()
 {
 	// 4 tehtava bonuksella
-	unsigned int i = 1;
+	unsigned int i = 1 , min = 0, max = 0;
 	float pituus = 0, pisteet[7] = { 0 };
-
 	std::cout << "Hypyn pituus: ";
 	std::cin >> pituus;
 
 	// i on alustettu numeroon 1 jotta loop toimisi järkevästi
 	while (i < tuomarit)
 	{
+
 		std::cout << "Tuomarin " << i << " pisteet: ";
 		std::cin >> pisteet[i];
+		if(pisteet[i] > pisteet[i-1] && i > 1)
+		{
+			max = pisteet[i];
+		}
+		else if (pisteet[i] < pisteet[i-1] && i > 1)
+		{
+			min = pisteet[i];
+		}
 		i++;
 	}
 
-	// Kommentoi virallinen jos tahdot epävirallisen
-	#ifdef Virallinen
-		pisteet[ltulos] = (pisteet[2] + pisteet[3] + pisteet[4]) + 0.9 * pituus;		
-	#else
-		pisteet[ltulos] = (pisteet[1] + pisteet[2] + pisteet[3] + pisteet[4] + pisteet[5]) + 0.9 * pituus;
-	#endif
+		pisteet[ltulos] = (pisteet[1] + pisteet[2] + pisteet[3] + pisteet[4] + pisteet[5] - max - min) + 0.9 * pituus;
 
 	std::cout << "Hypyn pisteet: " << pisteet[ltulos] << std::endl;
 
